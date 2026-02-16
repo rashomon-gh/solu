193477f06e4101543eebd3546398064d16d4ebc4
rashomon <shawon.ashraf@outlook.com>
2026-02-16 16:04:14 +0100

    solu module

---
+++ solu/module.py
@@ -0,0 +1,17 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class SoLU(nn.Module):
+    """
+    Softmax Linear Unit (SoLU) activation function.
+    Formula: f(x) = x * softmax(x)
+    """
+
+    def __init__(self, dim=-1):
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, x):
+        # Applies softmax along the last dimension (typically the feature dimension in Transformers)
+        return x * F.softmax(x, dim=self.dim)
